e9c026fde2e62a40378c4cf3bd0f4254df56e587
MateusLim4 <70021830+MateusLim4@users.noreply.github.com>
2021-10-01 22:24:00 -0300

    Add files via upload

---
+++ lib/modules/auth/auth_controller.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:peteat/modules/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthController {
+  UserModel? _user;
+
+  UserModel get user => _user!;
+
+  void SetUser(BuildContext context, UserModel? user) {
+    if (user != null) {
+      saveUser(user);
+      _user = user;
+      Navigator.pushReplacementNamed(context, "/home");
+    } else {
+      Navigator.pushReplacementNamed(context, "/login");
+    }
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("user", user.toJson());
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    await Future.delayed(Duration(seconds: 2));
+    if (instance.containsKey("user")) {
+      final json = instance.get("user") as String;
+      SetUser(context, UserModel.fromJson(json));
+      return;
+    } else {
+      SetUser(context, null);
+    }
+  }
+}
